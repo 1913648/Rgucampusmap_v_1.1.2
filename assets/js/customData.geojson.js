b4fa96ee3406b9bad662f3b97c60aee0ec524798
1913648 <60344170+1913648@users.noreply.github.com>
2020-03-06 16:59:46 +0800

    Add files via upload

---
+++ assets/js/customData.geojson.js
@@ -0,0 +1,86 @@
+var customData = {
+    'features': [
+        {
+            "geometry": {
+                "coordinates": [
+                    -2.1345204802807416,
+                    57.118596877616795
+                ],
+                "type": "Point"
+            },
+            "type": "Feature",
+            "properties": {
+                "title": "N106"
+            }
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    -2.134652958439432,
+                    57.118663768680705
+                ],
+                "type": "Point"
+            },
+            "type": "Feature",
+            "properties": {
+                "title": "N105",
+
+            }
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    -2.1343861201621053,
+                    57.11855622008005
+                ],
+                "type": "Point"
+            },
+            "type": "Feature",
+            "properties": {
+                "title": "N107"
+            }
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    -2.1342341390446506,
+                    57.11852512899293
+                ],
+                "type": "Point"
+            },
+            "type": "Feature",
+            "properties": {
+                "title": "N108"
+            }
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    -2.1340425106792225,
+                    57.11852154155798
+                ],
+                "type": "Point"
+            },
+            "type": "Feature",
+            "properties": {
+                "title": "N109"
+            }
+        },
+        {
+            "geometry": {
+                "coordinates": [
+                    -2.1338707059392448,
+                    57.11854306616172
+                ],
+                "type": "Point"
+            },
+            "type": "Feature",
+            "properties": {
+                "title": "N114"
+            }
+        },
+
+    ],
+
+    'type': 'FeatureCollection'
+};
